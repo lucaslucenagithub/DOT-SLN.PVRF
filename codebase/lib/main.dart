bcf6bc2ecc29a09babfd1715795d51167a09128f
Lucas Lucena <dayyumn@gmail.com>
2021-03-10 23:11:15 -0300

    removing windows support and more sections implementations

--- codebase/lib/main.dart
+++ codebase/lib/main.dart
@@ -1,8 +1,11 @@
+import 'dart:io';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 void main() {
   LicenseRegistry.addLicense(() async* {
@@ -130,7 +133,8 @@ class _MyHomePageState extends State<MyHomePage> {
                           margin: EdgeInsets.only(top: 40),
                           child: Text(
                             'Serviços',
-                            style: TextStyle(fontSize: 48, color: Colors.black),
+                            style: TextStyle(
+                                fontSize: 48, color: Colors.lightBlue.shade900),
                           ),
                         ),
                         Container(
@@ -287,18 +291,45 @@ class _MyHomePageState extends State<MyHomePage> {
               Container(
                 height: 800.0,
                 color: Colors.grey[50],
-                alignment: Alignment.center,
-                child: Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Text(
-                        'Sobre',
-                        style: TextStyle(fontSize: 48, color: Colors.black),
-                      ),
-                    ],
-                  ),
+                child: Row(
+                  children: [
+                    Expanded(
+                        child: Column(children: [
+                      Container(
+                          margin: EdgeInsets.only(top: 40, left: 50),
+                          alignment: Alignment.topLeft,
+                          child: Column(
+                            children: [
+                              Container(
+                                  child: Text(
+                                'Sobre',
+                                style: TextStyle(
+                                    fontSize: 48,
+                                    color: Colors.lightBlue.shade900),
+                              )),
+                              Container(
+                                margin: EdgeInsets.only(top: 5),
+                                height: 5,
+                                width: 100,
+                                color: Colors.amber[800],
+                              ),
+                            ],
+                          )),
+                      Container(
+                        padding: EdgeInsets.all(50),
+                        child: Text(
+                            'Lorem Ipsum is simply dummy text of the printing and typesetting industry. dummy text of the printing and typesetting industry. Lore. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s.'),
+                      )
+                    ])),
+                    Expanded(
+                        child: Container(
+                      decoration: BoxDecoration(
+                          image: DecorationImage(
+                              image: AssetImage(
+                                  '../assets/scaffolding/crane-construction-building.png'),
+                              fit: BoxFit.cover)),
+                    )),
+                  ],
                 ),
               ),
               Container(
@@ -307,30 +338,88 @@ class _MyHomePageState extends State<MyHomePage> {
                 alignment: Alignment.center,
                 child: Center(
                   child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      Text(
-                        'Contato',
-                        style: TextStyle(fontSize: 48, color: Colors.black),
-                      ),
+                      Container(
+                          margin: EdgeInsets.only(top: 40),
+                          child: Column(
+                            children: [
+                              Container(
+                                  child: Text(
+                                'Contato',
+                                style: TextStyle(
+                                    fontSize: 48,
+                                    color: Colors.lightBlue.shade900),
+                              )),
+                              Container(
+                                margin: EdgeInsets.only(top: 5),
+                                height: 5,
+                                width: 100,
+                                color: Colors.amber[800],
+                              ),
+                            ],
+                          )),
+                      Expanded(
+                          child: Container(
+                              margin: EdgeInsets.only(top: 20),
+                              child: WebView(
+                                initialUrl: Uri.dataFromString(
+                                  '<html><body><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14635.100183842442!2d-46.59440242267025!3d-23.504610777994305!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce5f4fafac8399%3A0xccbeef5f59ab61ef!2sVila%20Maria%20Alta%2C%20S%C3%A3o%20Paulo%20-%20State%20of%20S%C3%A3o%20Paulo!5e0!3m2!1sen!2sbr!4v1615425716177!5m2!1sen!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe></body></html>',
+                                  mimeType: 'text/html',
+                                ).toString(),
+                                javascriptMode: JavascriptMode.unrestricted,
+                              ))),
+                      Row(),
+                      Container(),
+                      Row()
                     ],
                   ),
                 ),
               ),
               Container(
-                height: 200.0,
+                padding: EdgeInsets.all(50),
+                height: 150.0,
                 color: Colors.grey[50],
                 alignment: Alignment.center,
                 child: Center(
-                  child: Column(
+                  child: Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      Text(
-                        'Rodapé',
-                        style: TextStyle(fontSize: 48, color: Colors.black),
-                      ),
+                      Expanded(
+                          flex: 4,
+                          child: RichText(
+                            text: TextSpan(
+                              text: '© PVRF Construtora. Criado por',
+                              style: TextStyle(
+                                  fontSize: 14,
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.w300),
+                              children: <TextSpan>[
+                                TextSpan(
+                                    text: ' .SLN',
+                                    style: TextStyle(
+                                        fontSize: 14,
+                                        color: Colors.red,
+                                        fontWeight: FontWeight.bold)),
+                              ],
+                            ),
+                          )),
+                      Expanded(
+                          flex: 1,
+                          child: ElevatedButton(
+                            child: Text(
+                              'Voltar ao topo',
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.w300),
+                            ),
+                            onPressed: () {},
+                            style: ButtonStyle(
+                                minimumSize: MaterialStateProperty.all<Size>(
+                                    Size(0, 80)),
+                                backgroundColor:
+                                    MaterialStateProperty.all(Colors.white)),
+                          ))
                     ],
                   ),
                 ),
