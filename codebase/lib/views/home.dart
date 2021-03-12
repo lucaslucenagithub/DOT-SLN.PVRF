098ffde7966e7d90acc3ebaa4b05620a6b724635
Lucas Lucena <dayyumn@gmail.com>
2021-03-12 04:08:34 -0300

    services responsiviness and refactoring

--- codebase/lib/views/home.dart
+++ codebase/lib/views/home.dart
@@ -1,42 +1,58 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:responsive_builder/responsive_builder.dart';
 
 class Home extends StatelessWidget {
   const Home({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.only(left: 150, right: 150),
-      height: 800.0,
-      decoration: BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage(
-                  '../assets/home-bg/beautiful-view-construction-site-city-during-sunset.png'),
-              fit: BoxFit.cover)),
-      alignment: Alignment.center,
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              'LOREM IPSUM DOLOR SIT AMET CONSECTETUR ADIPISCING ELIT',
-              style: TextStyle(
-                fontSize: 67,
-                color: Colors.white,
+    return ResponsiveBuilder(builder: (context, sizingInformation) {
+      double descriptionSize =
+          sizingInformation.deviceScreenType == DeviceScreenType.mobile
+              ? 36
+              : sizingInformation.deviceScreenType == DeviceScreenType.tablet
+                  ? 47
+                  : 67;
+
+      double paddingLeftRight =
+          sizingInformation.deviceScreenType == DeviceScreenType.mobile
+              ? 2
+              : 150;
+
+      return Container(
+        padding:
+            EdgeInsets.only(left: paddingLeftRight, right: paddingLeftRight),
+        height: 800.0,
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage(
+                    '../assets/home-bg/beautiful-view-construction-site-city-during-sunset.png'),
+                fit: BoxFit.cover)),
+        alignment: Alignment.center,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                'LOREM IPSUM DOLOR SIT AMET CONSECTETUR ADIPISCING ELIT',
+                style: TextStyle(
+                  fontSize: descriptionSize,
+                  color: Colors.white,
+                ),
+                textAlign: TextAlign.center,
               ),
-              textAlign: TextAlign.center,
-            ),
-            Icon(
-              FontAwesomeIcons.chevronDown,
-              color: Colors.white,
-              size: 80,
-            )
-          ],
+              Icon(
+                FontAwesomeIcons.chevronDown,
+                color: Colors.white,
+                size: 80,
+              )
+            ],
+          ),
         ),
-      ),
-    );
+      );
+    });
   }
 }
--- codebase/lib/views/services.dart
+++ codebase/lib/views/services.dart
@@ -1,170 +1,179 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
+import 'package:responsive_builder/responsive_builder.dart';
 
 class Services extends StatelessWidget {
   const Services({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.only(left: 50, right: 50),
-      height: 850.0,
-      color: Colors.white,
-      alignment: Alignment.center,
-      child: Center(
-        child: Column(
-          children: [
-            Column(children: [
-              Container(
-                margin: EdgeInsets.only(top: 40),
-                child: Text(
-                  'Serviços',
-                  style:
-                      TextStyle(fontSize: 48, color: Colors.lightBlue.shade900),
+    return ResponsiveBuilder(builder: (context, sizingInformation) {
+      Axis orientation = sizingInformation.deviceScreenType ==
+                  DeviceScreenType.mobile ||
+              sizingInformation.deviceScreenType == DeviceScreenType.tablet ||
+              sizingInformation.screenSize <= Size(1190, 1009)
+          ? Axis.vertical
+          : Axis.horizontal;
+
+      double paddingLeftRight =
+          sizingInformation.deviceScreenType == DeviceScreenType.tablet
+              ? 120
+              : sizingInformation.deviceScreenType == DeviceScreenType.mobile
+                  ? 2
+                  : 50;
+
+      return Expanded(
+        child: Container(
+            alignment: Alignment.center,
+            padding: EdgeInsets.only(
+                left: paddingLeftRight,
+                right: paddingLeftRight,
+                top: 10,
+                bottom: 50),
+            color: Colors.white,
+            child: Column(
+              children: [
+                Container(
+                  margin: EdgeInsets.only(top: 40),
+                  child: Text(
+                    'Serviços',
+                    style: TextStyle(
+                        fontSize: 48, color: Colors.lightBlue.shade900),
+                  ),
                 ),
-              ),
-              Container(
-                margin: EdgeInsets.only(top: 5, bottom: 40),
-                height: 5,
-                width: 100,
-                color: Colors.amber[800],
-              ),
-            ]),
-            Container(
-                margin: EdgeInsets.only(top: 40),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Expanded(
-                        child: Card(
-                      margin: EdgeInsets.only(right: 40, left: 40),
-                      child: Column(
-                        children: [
-                          Container(
-                            padding: EdgeInsets.only(left: 150, right: 150),
-                            height: 326.0,
-                            decoration: BoxDecoration(
-                                image: DecorationImage(
-                                    image: AssetImage(
-                                        '../assets/services/yellow-hat/construction-plan-calculator-hard-hat-project-isto.jpg'),
-                                    fit: BoxFit.cover)),
-                          ),
-                          Container(
-                            height: 250,
-                            padding: EdgeInsets.all(0),
-                            margin: EdgeInsets.all(0),
-                            child: Column(
-                              children: [
-                                Container(
-                                  child: Text(
-                                    'Projetos, construções e reformas',
-                                    style: TextStyle(
-                                        fontSize: 24,
-                                        fontWeight: FontWeight.normal,
-                                        color: Colors.lightBlue.shade900),
-                                    textAlign: TextAlign.start,
-                                  ),
-                                  margin: EdgeInsets.only(top: 20, bottom: 20),
-                                ),
-                                Text(
-                                    'Lorem Ipsum is simply dummy text of the printing and typesetting industry. dummy text of the printing and typesetting industry. Lore. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s.'),
-                              ],
+                Container(
+                  margin: EdgeInsets.only(top: 5, bottom: 40),
+                  height: 5,
+                  width: 100,
+                  color: Colors.amber[800],
+                ),
+                Flex(
+                    direction: orientation,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Card(
+                        margin:
+                            EdgeInsets.only(right: 40, left: 40, bottom: 40),
+                        child: Column(
+                          children: [
+                            Container(
+                              padding: EdgeInsets.only(left: 150, right: 150),
+                              height: 326.0,
+                              decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                      image: AssetImage(
+                                          '../assets/services/yellow-hat/construction-plan-calculator-hard-hat-project-isto.jpg'),
+                                      fit: BoxFit.cover)),
                             ),
-                            width: 240,
-                          )
-                        ],
-                      ),
-                      color: Colors.grey[50],
-                    )),
-                    Expanded(
-                        child: Card(
-                      margin: EdgeInsets.only(right: 40, left: 40),
-                      child: Column(
-                        children: [
-                          Container(
-                            padding: EdgeInsets.only(left: 150, right: 150),
-                            height: 326.0,
-                            decoration: BoxDecoration(
-                                image: DecorationImage(
-                                    image: AssetImage(
-                                        '../assets/services/structural/Steel_construction.jpg'),
-                                    fit: BoxFit.cover)),
-                          ),
-                          Container(
-                            height: 250,
-                            padding: EdgeInsets.all(0),
-                            margin: EdgeInsets.all(0),
-                            child: Column(
-                              children: [
-                                Container(
-                                  child: Text(
-                                    'Reforços estruturais',
-                                    style: TextStyle(
-                                        fontSize: 24,
-                                        fontWeight: FontWeight.normal,
-                                        color: Colors.lightBlue.shade900),
-                                    textAlign: TextAlign.start,
+                            Container(
+                              height: 250,
+                              child: Column(
+                                children: [
+                                  Container(
+                                    child: Text(
+                                      'Projetos, construções e reformas',
+                                      style: TextStyle(
+                                          fontSize: 24,
+                                          fontWeight: FontWeight.normal,
+                                          color: Colors.lightBlue.shade900),
+                                      textAlign: TextAlign.start,
+                                    ),
+                                    margin:
+                                        EdgeInsets.only(top: 20, bottom: 20),
                                   ),
-                                  margin: EdgeInsets.only(top: 20, bottom: 20),
-                                ),
-                                Text(
-                                    'Lorem Ipsum is simply dummy text of the printing and typesetting industry. dummy text of the printing and typesetting industry. Lore. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s.'),
-                              ],
-                            ),
-                            width: 240,
-                          )
-                        ],
+                                  Text(
+                                      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. dummy text of the printing and typesetting industry. Lore. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s.'),
+                                ],
+                              ),
+                              width: 240,
+                            )
+                          ],
+                        ),
+                        color: Colors.grey[50],
                       ),
-                      color: Colors.grey[50],
-                    )),
-                    Expanded(
-                        child: Card(
-                      margin: EdgeInsets.only(right: 40, left: 40),
-                      child: Column(
-                        children: [
-                          Container(
-                            padding: EdgeInsets.only(left: 150, right: 150),
-                            height: 326.0,
-                            decoration: BoxDecoration(
-                                image: DecorationImage(
-                                    image: AssetImage(
-                                        '../assets/services/writting-papper/laudos-pericias.jpg'),
-                                    fit: BoxFit.cover)),
-                          ),
-                          Container(
-                            height: 250,
-                            padding: EdgeInsets.all(0),
-                            margin: EdgeInsets.all(0),
-                            child: Column(
-                              children: [
-                                Container(
-                                  child: Text(
-                                    'Laudos e perícias',
-                                    style: TextStyle(
-                                        fontSize: 24,
-                                        fontWeight: FontWeight.normal,
-                                        color: Colors.lightBlue.shade900),
-                                    textAlign: TextAlign.start,
+                      Card(
+                        margin:
+                            EdgeInsets.only(right: 40, left: 40, bottom: 40),
+                        child: Column(
+                          children: [
+                            Container(
+                              padding: EdgeInsets.only(left: 150, right: 150),
+                              height: 326.0,
+                              decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                      image: AssetImage(
+                                          '../assets/services/structural/Steel_construction.jpg'),
+                                      fit: BoxFit.cover)),
+                            ),
+                            Container(
+                              height: 250,
+                              child: Column(
+                                children: [
+                                  Container(
+                                    child: Text(
+                                      'Reforços estruturais',
+                                      style: TextStyle(
+                                          fontSize: 24,
+                                          fontWeight: FontWeight.normal,
+                                          color: Colors.lightBlue.shade900),
+                                      textAlign: TextAlign.start,
+                                    ),
+                                    margin:
+                                        EdgeInsets.only(top: 20, bottom: 20),
                                   ),
-                                  margin: EdgeInsets.only(top: 20, bottom: 20),
-                                ),
-                                Text(
-                                    'Lorem Ipsum is simply dummy text of the printing and typesetting industry. dummy text of the printing and typesetting industry. Lore. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s.'),
-                              ],
-                            ),
-                            width: 240,
-                          )
-                        ],
+                                  Text(
+                                      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. dummy text of the printing and typesetting industry. Lore. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s.'),
+                                ],
+                              ),
+                              width: 240,
+                            )
+                          ],
+                        ),
+                        color: Colors.grey[50],
                       ),
-                      color: Colors.grey[50],
-                    )),
-                  ],
-                ))
-          ],
-        ),
-      ),
-    );
+                      Card(
+                        margin:
+                            EdgeInsets.only(right: 40, left: 40, bottom: 40),
+                        child: Column(
+                          children: [
+                            Container(
+                              padding: EdgeInsets.only(left: 150, right: 150),
+                              height: 326.0,
+                              decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                      image: AssetImage(
+                                          '../assets/services/writting-papper/laudos-pericias.jpg'),
+                                      fit: BoxFit.cover)),
+                            ),
+                            Container(
+                              height: 250,
+                              child: Column(
+                                children: [
+                                  Container(
+                                    child: Text(
+                                      'Laudos e perícias',
+                                      style: TextStyle(
+                                          fontSize: 24,
+                                          fontWeight: FontWeight.normal,
+                                          color: Colors.lightBlue.shade900),
+                                      textAlign: TextAlign.start,
+                                    ),
+                                    margin:
+                                        EdgeInsets.only(top: 20, bottom: 20),
+                                  ),
+                                  Text(
+                                      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. dummy text of the printing and typesetting industry. Lore. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s.'),
+                                ],
+                              ),
+                              width: 240,
+                            )
+                          ],
+                        ),
+                        color: Colors.grey[50],
+                      )
+                    ])
+              ],
+            )),
+      );
+    });
   }
 }
