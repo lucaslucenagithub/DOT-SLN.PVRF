42236f3197a1fec7616aae87835559f36ca81478
Lucas Lucena <dayyumn@gmail.com>
2021-03-16 13:50:56 -0300

    about section responsive

--- codebase/lib/views/about.dart
+++ codebase/lib/views/about.dart
@@ -13,47 +13,65 @@ class About extends StatelessWidget {
             ? 42
             : 48;
 
+    double descriptionSize = MediaQuery.of(context).size.width <= 600
+        ? 12
+        : MediaQuery.of(context).size.width >= 601 &&
+                MediaQuery.of(context).size.width <= 1048
+            ? 14
+            : 18;
+
+    Axis axisDirection = MediaQuery.of(context).size.width <= 520
+        ? Axis.vertical
+        : Axis.horizontal;
+
+    double sectionHeigh = MediaQuery.of(context).size.width <= 376 ? 900 : 800;
+
     return Container(
-      height: 800.0,
+      height: sectionHeigh,
       color: Colors.grey[50],
-      child: Row(
+      child: Flex(
+        direction: axisDirection,
         children: [
           Expanded(
+              flex: 2,
               child: Column(children: [
-            Container(
-                margin: EdgeInsets.only(top: 40, left: 50),
-                alignment: Alignment.topLeft,
-                child: Column(
-                  children: [
-                    Container(
-                        child: Text(
-                      'Sobre',
-                      style: TextStyle(
-                          fontSize: titleSize,
-                          color: Colors.lightBlue.shade900),
+                Container(
+                    margin: EdgeInsets.only(top: 40, left: 50),
+                    alignment: Alignment.topLeft,
+                    child: Column(
+                      children: [
+                        Container(
+                            child: Text(
+                          'Sobre',
+                          style: TextStyle(
+                              fontSize: titleSize,
+                              color: Colors.lightBlue.shade900),
+                        )),
+                        Container(
+                          margin: EdgeInsets.only(top: 5),
+                          height: 5,
+                          width: 100,
+                          color: Colors.amber[800],
+                        ),
+                      ],
                     )),
-                    Container(
-                      margin: EdgeInsets.only(top: 5),
-                      height: 5,
-                      width: 100,
-                      color: Colors.amber[800],
-                    ),
-                  ],
-                )),
-            Container(
-              padding: EdgeInsets.all(50),
-              child: Text(
-                  'Lorem Ipsum is simply dummy text of the printing and typesetting industry. dummy text of the printing and typesetting industry. Lore. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s.Lorem Ipsum is simply dummy text of the printing and typesetting industry. dummy text of the printing and typesetting industry. Lore. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s.Lorem Ipsum is simply dummy text of the printing and typesetting industry. dummy text of the printing and typesetting industry. Lore. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s.'),
-            )
-          ])),
+                Container(
+                  padding: EdgeInsets.all(50),
+                  child: Text(
+                    'Lorem Ipsum is simply dummy text of the printing and typesetting industry. dummy text of the printing and typesetting industry. Lore. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s.Lorem Ipsum is simply dummy text of the printing and typesetting industry. dummy text of the printing and typesetting industry. Lore. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s.Lorem Ipsum is simply dummy text of the printing and typesetting industry. dummy text of the printing and typesetting industry. Lore. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s.',
+                    style: TextStyle(fontSize: descriptionSize),
+                  ),
+                )
+              ])),
           Expanded(
+              flex: 1,
               child: Container(
-            decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage(
-                        '../assets/scaffolding/crane-construction-building.png'),
-                    fit: BoxFit.cover)),
-          )),
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage(
+                            '../assets/scaffolding/crane-construction-building.png'),
+                        fit: BoxFit.cover)),
+              )),
         ],
       ),
     );
